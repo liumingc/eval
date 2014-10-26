fbed6535fe764cfaa4d67a9c08adc9b710c045dd
liumingc <inferno_ch@163.com>
2014-10-26 20:46:26 +0800

    add stack version compiler

---
+++ stack-v1.scm
@@ -0,0 +1,136 @@
+;;; common utilities
+(use-modules (ice-9 format))
+(use-modules (ice-9 pretty-print))
+
+(define-syntax record
+  (syntax-rules ()
+    ((_ fields var body ...)
+     (apply (lambda fields body ...) var))))
+
+(define-syntax record-case
+  (lambda (exp)
+    (syntax-case exp ()
+      ((_ exp c1 c2 ...)
+       #`(let ((x exp))
+	   (cond
+	    #,@(let f ((c1 #'c1)
+		       (c2 #'(c2 ...)))
+		 (if (null? c2)
+		     (syntax-case c1 (t) ; t as else
+		       ((t body ...)
+			#'((else body ...)))
+		       ((k fields body ...)
+			#'(((eqv? (car x) 'k)
+			    (record fields (cdr x) body ...)))))
+		     (syntax-case c1 ()
+		       ((k fields body ...)
+			#`(((eqv? (car x) 'k)
+			    (record fields (cdr x) body ...))
+			   #,@(f (car c2) (cdr c2)))))))))))))
+
+
+;;; functional can't be called outside it's lex-scope
+(define functional
+  (lambda (body e)
+    (list body e)))
+
+;;; stack
+(define stack (make-vector 8000))
+
+(define push
+  (lambda (x s)
+    (vector-set! stack s x)
+    (+ s 1)))
+
+;;; (index sp 0) -> pc (index sp 1) -> arg0 ...
+(define index
+  (lambda (s i)
+    (vector-ref stack (- (- s i) 1))))
+
+(define index-set!
+  (lambda (s i v)
+    (vector-set! stack (- (- s i) 1) v)))
+
+(define comp
+  (lambda (x e next)
+    (cond
+     ((symbol? x)
+      (comp-lookup x e
+		   (lambda (n m)
+		     (list 'refer n m next))))
+     ((pair? x)
+      (record-case x
+		   (quote (obj)
+			  (list 'const obj next))
+		   (lambda (vars body)
+		     (list 'close
+			   (comp body
+				 (extend e vars)
+				 (list 'return (+ (length vars) 1)))
+			   next))
+		   (if (test then else)
+		       (let ((thenc (comp then e next))
+			     (elsec (comp else e next)))
+			 (comp test e `(test ,thenc ,elsec))))
+		   (set! (var x)
+			 (comp-lookup var e
+				      (lambda (n m)
+					(comp x e `(assign ,n ,m ,next)))))
+		   (t (let f ((args (cdr x))
+			      (c (comp (car x) e '(apply))))
+			(if (null? args)
+			    (list 'frame next c)
+			    (f (cdr args)
+			       (comp (car args) e (list 'arg c))))))))
+     (else (list 'const x next)))))
+
+(define comp-lookup
+  (lambda (var e fn)
+    (let nextrib ((e e) (rib 0))
+      (let nextelt ((vars (car e))
+		    (elt 0))
+	(cond
+	 ((null? vars) (nextrib (cdr e) (+ rib 1)))
+	 ((eq? var (car vars)) (fn rib elt))
+	 (else (nextelt (cdr vars) (+ elt 1))))))))
+
+(define extend
+  (lambda (e r)
+    (cons r e)))
+
+;;; link is env link, static link
+(define VM
+  (lambda (a x e s)
+    (record-case x
+		 (halt () a)
+		 (refer (n m x)
+			(VM (index (find-link n e) m) x e s))
+		 (const (obj x)
+			(VM obj x e s))
+		 (assign (n m x)
+			 (VM (index-set! (find-link n e) m a) x e s))
+		 (test (yes no)
+		       (VM a (if a yes no) e s))
+		 (close (body x)
+			(VM (functional body e) x e s))
+		 (frame (ret x)
+			(VM a x e (push ret (push e s))))
+		 (arg (x)
+		      (VM a x e (push a s)))
+		 (apply ()
+			(record (body link) a
+				(VM a body s (push link s))))
+		 (return (n)
+			 (let ((s (- s n)))
+			   (VM a (index s 0) (index s 1) (- s 2)))))))
+
+
+(define find-link
+  (lambda (n e)
+    (if (= n 0)
+	e
+	(find-link (- n 1) (index e -1)))))
+
+(define evaluate
+  (lambda (x)
+    (VM '() (comp x '() '(halt)) 0 0)))
--- stack-v2.scm
+++ stack-v2.scm
@@ -1,5 +1,3 @@
-;;; add primitive proc
-;;; add begin, let form
 
 (use-modules (ice-9 format))
 (use-modules (ice-9 pretty-print))
@@ -14,35 +12,31 @@
 
 
 (define extend
-  (lambda (env vars vals)
-    (cons (cons vars vals) env)))
+  (lambda (env vars)
+    (cons vars env)))
 
 (define lookup
-  (lambda (var e)
-    (let nextrib ((e e))
-      (if (null? e)
-	  (error "var not defined" var))
-      (let nextelt ((vars (caar e))
-		    (vals (cdar e)))
+  (lambda (access e)
+    (let ((vars (list-ref e (car access))))
+      (let f ((r (car e))
+	      (elt (cdr access)))
 	(cond
-	 ((null? vars) (nextrib (cdr e)))
-	 ((eq? (car vars) var) vals)
-	 (else (nextelt (cdr vars) (cdr vals))))))))
+	 ((zero? elt) r)
+	 (else (f (cdr r) (- elt 1))))))))
 
+;;; (fn (x) (fn (y) (+ x y)))
+;;; -> (fn (x) (fn (y) (+ (1 . 0) (0 . 0)))
+(define comp-lookup
+  (lambda (var e)
+    (let nextrib ((e e) (rib 0))
+      (let nextelt ((vars (car e)) (elt 0))
+	(cond
+	 ((null? vars) (nextrib (cdr e) (+ rib 1)))
+	 ((eq? var (car vars)) (cons rib elt))
+	 (else (nextelt (cdr vars) (+ elt 1))))))))
 
 
 (define env '())
-
-(define init
-  (lambda ()
-    (set! env
-	  (extend env
-		  '(+ - * / cons car cdr
-		      pair? = < zero?)
-		  (list + - * / cons car cdr
-			pair? = < zero?)))))
-
-(init)
 
 
 ;;; vm registers(5)
@@ -76,27 +70,27 @@
 
 ;;; compile, name comp to avoid name conflict.
 (define comp
-  (lambda (x next)
+  (lambda (x e next)
     (cond
      ((symbol? x)
-      `(refer ,x ,next))
+      `(refer ,(comp-lookup x e) ,next))
      ((pair? x)
       (record-case x
 		   (quote (obj)
 			  `(const ,obj ,next))
 		   (lambda (vars . body)
-		     (list 'close vars (comp `(begin ,@body) '(return)) next))
+		     (list 'close (comp `(begin ,@body) (extend e vars) '(return)) next))
 		   (if (test then else)
-		       (comp test
+		       (comp test e
 			     `(test
-			       ,(comp then next)
-			       ,(comp else next))))
+			       ,(comp then e next)
+			       ,(comp else e next))))
 		   (set! (var val)
-			 (comp val `(assign ,var ,next)))
+			 (comp val e `(assign ,(comp-lookup var e) ,next)))
 		   (call/cc (x)
 			    (let ((c `(conti
 				       (arg
-					,(comp x '(apply))))))
+					,(comp x e '(apply))))))
 			      (if (tail? next)
 				  c
 				  `(frame ,next ,c))))
@@ -104,28 +98,29 @@
 			  (let recur ((exps exps))
 			    (cond
 			     ((null? (cdr exps))
-			      (comp (car exps) next))
+			      (comp (car exps) e next))
 			     (else
-			      (comp (car exps) (recur (cdr exps)))))))
+			      (comp (car exps) e (recur (cdr exps)))))))
 		   (let (binds . exps)
 		     (comp `((lambda ,(map car binds)
-			       (begin ,@exps)) ,@(map cadr binds)) next))
+			       ,@exps) ,@(map cadr binds)) e next))
 		   (let* (binds . exps)
 		     (comp `((lambda ,(reverse (map car binds))
-			       (begin ,@exps)) ,@(reverse (map cadr binds))) next))
+			       ,@exps) ,@(reverse (map cadr binds))) e next))
 		   (read ()
 			 (list 'read next))
 		   (write (obj)
 			  (list 'write obj next))
 		   (t
 		    (let loop ((args (cdr x))
-			       (c (comp (car x) '(apply))))
+			       (c (comp (car x) e '(apply))))
 		      (if (null? args)
 			  (if (tail? next)
 			      c
 			      `(frame ,next ,c))
 			  (loop (cdr args)
 				(comp (car args)
+				      e
 				      `(arg ,c))))))))
      (else
       `(const ,x ,next)))))
@@ -148,33 +143,32 @@
 	((const refer assign write)
 	 (format #t "~a ~a~%" (list-ref x 0) (list-ref x 1)))))))
 
-;;; SECD machine? I don't known semantics.
-;;; The dragon book don't mention this.
+
+;;; diff: frame, ret
 (define VM
   (lambda (a x e r s)
     (when *debug*
 	  #;(print x)
-	  (decode x)
-	  )
+	  (decode x))
     (record-case x
 		 (halt () a)
-		 (refer (var x)
-			(VM (car (lookup var e)) x e r s))
+		 (refer (access x)
+			(VM (car (lookup access e)) x e r s))
 		 (const (obj x)
 			(VM obj x e r s))
-		 (close (vars body x)
-			(VM (closure vars body e) x e r s))
+		 (close (body x)
+			(VM (closure body e) x e r s))
 		 (test (then else)
 		       (VM a (if a then else) e r s))
-		 (assign (var x)
-			 (set-car! (lookup var e) a)
+		 (assign (access x)
+			 (set-car! (lookup access e) a)
 			 (VM a x e r s))
 		 (conti (x)
 			(VM (continuation s) x e r s))
 		 (nuate (s var)
 			(VM (car (lookup var e)) '(return) e r s))
 		 (frame (ret x)
-			(VM a x e '() (call-frame ret e r s)))
+			(VM a x e '() (push ret (push e (push r s)))))
 		 (arg (x)
 		      (VM a x e (cons a r) s))
 		 (apply ()
@@ -183,12 +177,10 @@
 			  (record (x e rib s) s
 				  (VM (apply a r) x e rib s)))
 			 (else
-					;(format #t "rib: ~a~%" r)
-			  (record (vars body e) a
-				  (VM a body (extend e vars r) '() s)))))
+			  (record (body e) a
+				  (VM a body (extend e r) '() s)))))
 		 (return ()
-			 (record (x e r s) s
-				 (VM a x e r s)))
+			 (VM a (index s 0) (index s 1) (index s 2) (- s 3)))
 		 (read (x)
 		       (VM (read) x e r s))
 		 (write (obj x)
@@ -198,43 +190,54 @@
 
 
 (define closure
-  (lambda (vars body e)
-    (list vars body e)))
+  (lambda (body e)
+    (list body e)))
 
 
 (define continuation
   (lambda (s)
-    (closure '(v) (list 'nuate s 'v) '())))
+    (closure
+     (list 'refer 0 0 (list 'nuate (save-stack s) '(return)))
+     '())))
 
-(define call-frame
-  (lambda (x e r s)
-    (list x e r s)))
+;;; stack utils
+(define stack-sz 2048)
+(define stack (make-vector stack-sz))
+
+(define (push val s)
+  (vector-set! stack s val)
+  (1+ s))
+
+(define index
+  (lambda (s i)
+    (vector-ref stack (1- (- s i)))))
+
+(define index-set!
+  (lambda (s i v)
+    (vector-set! stack (1- (- s i)) v)))
+
+;;; continuation has to copy the whole stack
+(define (save-stack s)
+  (let ((v (make-vector stack-sz)))
+    (let copy ((i 0))
+      (when (< i s)
+	(vector-set! v i (vector-ref s i))
+	(copy (1+ i))))
+    v))
+
+(define (restore-stack v)
+  (let ((l (vetor-length v)))
+    (let copy ((i 0))
+      (when (< i l)
+	(vector-set! stack i (vector-ref v i))
+	(copy (1+ i))))
+    l))
+
 
 (define go
   (lambda (x)
-    (let ((code (comp x '(halt))))
+    (let ((code (comp x '() '(halt))))
       (if *debug*
 	  (pretty-print code))
-      (VM '() code env '() '()))))
-
-#|
-(go '(let ((f #f))
-       (set! f (lambda (sum n)
-		 (if (= n 0)
-		     sum
-		     (f (+ sum n) (- n 1)))))
-       (f 0 10)))
-|#
-
-(go '(let ((counter 0))
-       (let* ((yin
-	       ((lambda (cc) (write "@") cc)
-		(call/cc (lambda (c) c))))
-	      (yang
-	       ((lambda (cc) (write "*") cc)
-		(call/cc (lambda (c) c)))))
-	 (set! counter (+ counter 1))
-	 (if (< counter 30)
-	     (yin yang)
-	     "end"))))
+      (VM '() code env '() 0))))
 
