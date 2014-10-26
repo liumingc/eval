80c607fc57137a4a8f6b517b7fe3192d392cde2b
liumingc <inferno_ch@163.com>
2014-10-26 20:49:22 +0800

    more

--- stack-v1.scm
+++ stack-v1.scm
@@ -2,31 +2,7 @@
 (use-modules (ice-9 format))
 (use-modules (ice-9 pretty-print))
 
-(define-syntax record
-  (syntax-rules ()
-    ((_ fields var body ...)
-     (apply (lambda fields body ...) var))))
-
-(define-syntax record-case
-  (lambda (exp)
-    (syntax-case exp ()
-      ((_ exp c1 c2 ...)
-       #`(let ((x exp))
-	   (cond
-	    #,@(let f ((c1 #'c1)
-		       (c2 #'(c2 ...)))
-		 (if (null? c2)
-		     (syntax-case c1 (t) ; t as else
-		       ((t body ...)
-			#'((else body ...)))
-		       ((k fields body ...)
-			#'(((eqv? (car x) 'k)
-			    (record fields (cdr x) body ...)))))
-		     (syntax-case c1 ()
-		       ((k fields body ...)
-			#`(((eqv? (car x) 'k)
-			    (record fields (cdr x) body ...))
-			   #,@(f (car c2) (cdr c2)))))))))))))
+(load "utils.scm")
 
 
 ;;; functional can't be called outside it's lex-scope
--- v1.scm
+++ v1.scm
@@ -1,55 +1,10 @@
-
-(define print
-  (lambda (x)
-    (display x)
-    (newline)))
-
-(define-syntax recur
-  (syntax-rules ()
-    ((_ f ((v e) ...) b ...)
-     (let ((f #f))
-       (set! f
-	     (lambda (v ...) b ...))
-       (f e ...)))))
-
-;;; destructuring-bind in CL
-(define-syntax record
-  (syntax-rules ()
-    ((_ fields var body ...)
-     (apply (lambda fields body ...) var))))
-
-
-
-(define-syntax record-case
-  (lambda (exp)
-    (syntax-case exp ()
-      ((_ exp c1 c2 ...)
-       #`(let ((x exp))
-	   ;(print x)
-	   (cond
-	    #,@(let f ((c1 #'c1)
-		       (c2 #'(c2 ...)))
-		 (if (null? c2)
-		     (syntax-case c1 (t) ; t as else
-		       ((t body ...)
-			#'((else body ...)))
-		       ((k fields body ...)
-			#'(((eqv? (car x) 'k)
-			    (record fields (cdr x) body ...)))))
-		     (syntax-case c1 ()
-		       ((k fields body ...)
-			#`(((eqv? (car x) 'k)
-			    (record fields (cdr x) body ...))
-			   #,@(f (car c2) (cdr c2)))))))))))))
-
+(load "utils.scm")
 
 (define stream-ref
   (lambda (s n)
     (if (= n 0)
 	(car s)
 	(stream-ref ((cdr s)) (- n 1)))))
-
-
 
 (define-syntax coroutine
   (syntax-rules ()
