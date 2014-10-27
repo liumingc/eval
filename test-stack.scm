ce6a25a46ba0f6d5b332816709a982fb838855e2
liumingc <inferno_ch@163.com>
2014-10-28 00:23:34 +0800

    add test

---
+++ test-stack.scm
@@ -0,0 +1,58 @@
+(use-modules (ice-9 format))
+#;(use-modules (ice-9 pretty-print))
+
+(load "stack-v3.scm")
+
+(define test-data
+  '((3 3)
+    ((quote (3 4)) (3 4))
+    ("hello" "hello")
+    ((lambda (x) x) "#<procedure>")
+    (((lambda (x) x) 5) 5)
+    (((lambda (f x) (f x)) (lambda (x) x) "-func-") "-func-")
+    ((if #f "true" "false") "false")
+    ((if #t "true" "false") "true")
+    ((if 0 "true" "false") "true")
+#;
+    ((let ((kons (lambda (ka kd)
+    (lambda (key)
+    (if (eq? key 'kar)
+    ka
+    kd))))
+    (kar (lambda (k)
+    (k 'kar)))
+    (kdr (lambda (k)
+    (k 'kdr))))
+    (kar (kons 3 4))) 3)
+    ))
+
+(define test
+  (lambda (exp res)
+    (let ((x (go exp)))
+      (if (equal? x res)
+	  (begin
+	    (format #t "~a -- passed~%" exp)
+	    #t)
+	  (begin
+	    (format #t "~a ~s -- failed~%" exp x)
+	    #f)))))
+
+(define test-case
+  (lambda ()
+    (let ((tot 0)
+	  (passed 0))
+      (for-each
+       (lambda (x)
+	 ;(record (exp res) x
+		 (set! tot (+ tot 1))
+		 (if (test (car x) (cadr x))
+		     (set! passed (+ passed 1)))
+		 ;)
+		 )
+      test-data)
+      (format #t " ---~%passed/total: ~a/~a~%"
+	      passed tot))))
+
+
+(format #t "ello~%")
+(test-case)
